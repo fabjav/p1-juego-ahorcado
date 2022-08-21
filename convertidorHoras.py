029702e879a85534b95631acaff568a1e8dd96b6
Fabio Javier Flores <fabiojflores98@gmail.com>
2022-08-21 14:38:50 -0300

    quise hacerlo con diccionarios. no funcionó

--- convertidorHoras.py
+++ convertidorHoras.py
@@ -1,35 +1,13 @@
-listMin = []
-listSeg = []
-sumaSeg = 0
-sumaMin = 0
-
-
-def convMin(min):
-    listMin.append(min)
-    for i in listMin:
-        sumaSeg = sumaSeg + i
-
-def convSeg(seg):
-     listSeg.append(seg)
-     for i in listSeg:
-         sumaMin = sumaMin + i
-
-while True: #corre el programa
-    min = int(input("min > "))
-    if min <60 and min>0:
-        convMin(min)
-    elif min == 111:
+Tiempos = { }
+while True:
+    minutos = int(input("min > "))
+    segundos = int(input("seg > "))
+    if 0<minutos<60 and 0<segundos<60:
+        Tiempos[minutos]=segundos
+    elif minutos == 111 or segundos == 111:
         break
-
     else:
-        print("error 404.")
+        print("error 506.")
+print(Tiempos)
 
-    seg = int(input("seg > "))
-    if seg <60 and seg>0:
-        convSeg(seg)
-    elif seg == 111:
-        break
 
-    else:
-        print("error 404.")
-    print("ingrese un 111 para terminar")

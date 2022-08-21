fed9d6a877ca97385cf4208716c9b302207fc09a
Fabio Javier Flores <fabiojflores98@gmail.com>
2022-08-21 15:33:24 -0300

    algoritmo terminado. No es elegante, pero funciona al 100%

--- convertidorHoras.py
+++ convertidorHoras.py
@@ -9,25 +9,27 @@ while True:
     else:
         break
 
-    print(minutos)
-    print(segundos)
+    #print(minutos)
+    #print(segundos)
 
 def sc_min(minutos):
     suma_mins = 0
     for i in minutos:
         suma_mins = suma_mins + i
     min_hora = suma_mins/60
-    print(min_hora)
+    return min_hora
 
 def sc_seg(segundos):
     suma_segs = 0
     for i in segundos:
         suma_segs = suma_segs + i
     seg_Min = suma_segs/60
-    print(seg_Min)
+    return seg_Min
 
-
+horas = sc_min(minutos)
+minutos = sc_seg(segundos)
 
 opción = input("> ").lower()
 if opción == "mostrar":
+    print(f"{horas:.0f}h {minutos:.0f}'")
 

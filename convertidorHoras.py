74a0d1b245f53770af3183e8e0ec649966e7d1d4
Fabio Javier Flores <fabiojflores98@gmail.com>
2022-08-21 15:01:02 -0300

    el algoritmo funciona. Falta que muestra la hora con el formato de reloj

--- convertidorHoras.py
+++ convertidorHoras.py
@@ -1,13 +1,33 @@
-Tiempos = { }
+minutos = []
+segundos = []
 while True:
-    minutos = int(input("min > "))
-    segundos = int(input("seg > "))
-    if 0<minutos<60 and 0<segundos<60:
-        Tiempos[minutos]=segundos
-    elif minutos == 111 or segundos == 111:
-        break
+    min = int(input("min > "))
+    seg = int(input("seg > "))
+    if 0<min<60 and 0<seg<60:
+        minutos.append(min)
+        segundos.append(seg)
     else:
-        print("error 506.")
-print(Tiempos)
+        break
 
+    print(minutos)
+    print(segundos)
+
+def sc_min(minutos):
+    suma_mins = 0
+    for i in minutos:
+        suma_mins = suma_mins + i
+    min_hora = suma_mins/60
+    print(min_hora)
+
+def sc_seg(segundos):
+    suma_segs = 0
+    for i in segundos:
+        suma_segs = suma_segs + i
+    seg_Min = suma_segs/60
+    print(seg_Min)
+
+
+
+opción = input("> ").lower()
+if opción == "mostrar":
 

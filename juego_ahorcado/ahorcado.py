1e8173be98594f574953c0f4e24c9220da7b8e25
Fabio Javier Flores <fabiojflores98@gmail.com>
2022-08-27 04:34:28 -0300

    funcion main agregada exitosamente. ahora el juego recinicia de una manera funcional y eficiente

--- juego_ahorcado/ahorcado.py
+++ juego_ahorcado/ahorcado.py
@@ -1,131 +1,148 @@
 
+def main():
+    
+    import random
+    import os
 
-import random
-import os
-
-palabras = ['teclado', 'mouse', 'monitor','gabinete','internet']
-n = random.choice(palabras) #selecciona una palabra al azar de la lista
-#print(n)
+    palabras = ['teclado', 'mouse', 'monitor','gabinete','internet']
+    n = random.choice(palabras) #selecciona una palabra al azar de la lista
+    #print(n)
 
 
-dibujo = ['''
-      +---+
-      |   |
-          |
-          |
-          |
-          |
-    =========''', '''
-      +---+
-      |   |
-      O   |
-          |
-          |
-          |
-    =========''', '''
-      +---+
-      |   |
-      O   |
-      |   |
-          |
-          |
-    =========''', '''
-      +---+
-      |   |
-      O   |
-     /|   |
-          |
-          |
-    =========''', '''
-      +---+
-      |   |
-      O   |
-     /|\  |
-          |
-          |
-    =========''', '''
-      +---+
-      |   |
-      O   |
-     /|\  |
-     /    |
-          |
-    =========''', '''
-      +---+
-      |   |
-      O   |
-     /|\  |
-     / \  |
-          |
-    =========''']
+    dibujo = ['''
+        +---+
+        |   |
+            |
+            |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+            |
+            |
+            |
+        =========''', '''
+        +---+
+        |   |
+        O   |
+        |   |
+            |
+            |
+        =========''', '''
+         +---+
+         |   |
+         O   |
+        /|   |
+             |
+             |
+        =========''', '''
+         +---+
+         |   |
+         O   |
+        /|\  |
+             |
+             |
+        =========''', '''
+         +---+
+         |   |
+         O   |
+        /|\  |
+        /    |
+             |
+        =========''', '''
+         +---+
+         |   |
+         O   |
+        /|\  |
+        / \  |
+             |
+        =========''']
 
-def fun_guiones(n):
-    palabra_lista = []
-    guiones = []
-    comparacion = []
-    for i in n:
-        guiones.append("_")
-    for i in n:
-        palabra_lista.append(i)
-    for i in n:
-        comparacion.append(i)
+    def fun_guiones(n):
+        palabra_lista = []
+        guiones = []
+        comparacion = []
+        for i in n:
+            guiones.append("_")
+        for i in n:
+            palabra_lista.append(i)
+        for i in n:
+            comparacion.append(i)
+            
+
+        
+        return guiones,palabra_lista,comparacion
         
 
-    
-    return guiones,palabra_lista,comparacion
-    
+    g,p,c = fun_guiones(n)
 
-g,p,c = fun_guiones(n)
-
-os.system("cls")
-fun_guiones(n)
-
-print(g)
-
-
-def letraSi(l):
-    while l in p:
-        ind = p.index(l) #tomar ese indice y guardarlo en ind
-        g[ind]=l # se cambia el guión por la letra según el ind
-        p[ind] = "0"
-    print(g)
-intentos = 1
-def letraNo(l,intentos):
-    
-    print(dibujo[intentos])
-    
-    print(f"fallos: {intentos}")
-    print(g)
-
-
-
-
-while True:
-    
-    l = input("ingrese letra > ").lower()
     os.system("cls")
-    if l in p:
-        letraSi(l)
-        if g == c:
-            print("Felicitaciones!")
-            break
-    else:
-        letraNo(l,intentos)
-        intentos += 1
-    if intentos == 7:
-        print("\tGame over!")
-        print(" ")
-        respuesta = input("reintentar? si/no > ")
-        if respuesta == "no":
-            print("""
-            programa finalizado
+    print(dibujo[0])
+    fun_guiones(n)
 
+    print(g)
+
+
+    def letraSi(l):
+        while l in p:
+            ind = p.index(l) #tomar ese indice y guardarlo en ind
+            g[ind]=l # se cambia el guión por la letra según el ind
+            p[ind] = "0"
+        print(g)
+    intentos = 1
+    def letraNo(l,intentos):
+        
+        print(dibujo[intentos])
+        
+        print(f"fallos: {intentos}")
+        print(g)
+
+
+
+
+    while True:
+        
+        l = input("ingrese letra > ").lower()
+        os.system("cls")
+        if l in p:
+            letraSi(l)
+            if g == c:
+                print("\n\t\tFelicitaciones!")
+                print(" ")
+                while True:
+                    respuesta1 = input("jugar de nuevo? si/no > ")
+                    if respuesta1 == "si":
+                        main()
+                    elif respuesta1 == "no":
+                        break
+                    else: 
+                        print("error de codigo")
+
+
+        else:
+            letraNo(l,intentos)
+            intentos += 1
+        if intentos == 7:
+            print("\tGame over!")
+            print(" ")
             
-            \t\tby Fabio Javier Flores""")
-            break
-        elif respuesta == "si":
-            
-            
+            while True:
+                respuesta = input("reintentar? si/no > ")
+                if respuesta == "si":
+                    main()
+                elif respuesta == "no":
+                    break
+                else: 
+                    print("error de codigo")
+                
+                
+                
+             
+
+                    
+main()           
                 
                 
 

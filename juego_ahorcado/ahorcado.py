a4723185852fd7681acfbe223e38f6e7bba488d1
Fabio Javier Flores <fabiojflores98@gmail.com>
2022-08-27 00:22:14 -0300

    funcion de game over funcionando al 100%. Falta funcion cls

--- juego_ahorcado/ahorcado.py
+++ juego_ahorcado/ahorcado.py
@@ -60,33 +60,53 @@ dibujo = ['''
 def fun_guiones(n):
     palabra_lista = []
     guiones = []
+    comparacion = []
     for i in n:
         guiones.append("_")
     for i in n:
         palabra_lista.append(i)
+    for i in n:
+        comparacion.append(i)
+        
+
     
-    return guiones,palabra_lista
+    return guiones,palabra_lista,comparacion
     
 
-g,p = fun_guiones(n)
+g,p,c = fun_guiones(n)
 
 fun_guiones(n)
 print(g)
 
-def fun_letra(l): #se pide una letra al usuario
-    
-    while True:
-        if l in p: #si l ingresada está en palabra_lista
-            ind = p.index(l) #tomar ese indice y guardarlo en ind
-            g[ind]=l # se cambia el guión por la letra según el ind
-            p[ind] = "0"
-                    
-        else:
-            
-            break
+
+def letraSi(l):
+    while l in p:
+        ind = p.index(l) #tomar ese indice y guardarlo en ind
+        g[ind]=l # se cambia el guión por la letra según el ind
+        p[ind] = "0"
     print(g)
+intentos = 1
+def letraNo(l,intentos):
+    
+    print(dibujo[intentos])
+    
+    print(f"fallos: {intentos}")
+
+
+
 
 while True:
-    fun_letra(input(">"))
+    l = input("> ")
+    if l in p:
+        letraSi(l)
+        if g == c:
+            print("Felicitaciones!")
+    else:
+        letraNo(l,intentos)
+        intentos += 1
+        if intentos == 7:
+            print("\tGame over!")
+            break
+
     
     
